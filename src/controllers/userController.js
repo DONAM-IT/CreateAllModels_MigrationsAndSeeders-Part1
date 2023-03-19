c9f5eccd2d4edfd620f6c7b5d1f454289fe5d119
DONAM-IT <doducnam.it@gmail.com>
2023-03-19 14:54:29 +0700

    seventh - 35 wirte REST APIs commit

---
+++ src/controllers/userController.js
@@ -0,0 +1,34 @@
+import userService from "../services/userService";
+
+let handleLogin = async (req, res) => {
+  let email = req.body.email;
+  //   console.log("your email: " + email); //email undefined
+  let password = req.body.password;
+  //   if(email === '' || email === null || email === 'undefined')
+  if (!email || !password) {
+    return res.status(500).json({
+      errCode: 1, //dinh danh 1 cai errCode tra ve 1l
+      message: "Missing inputs parameter!",
+    });
+  }
+
+  let userData = await userService.handleUserLogin(email, password);
+  //check email exist
+  //compare password
+  // return userInfor
+  // access_token: JWT json web token
+  return res.status(200).json({
+    errCode: userData.errCode,
+    message: userData.errMessage,
+    user: userData.user ? userData.user : {}, //nếu mà ko có biến user thì trả về giá trị object rỗng, có thì in ra
+    // userData,
+    // errCode: 0,
+    // message: "hello world",
+    // yourEmail: email,
+    // test: "test",
+  });
+};
+
+module.exports = {
+  handleLogin: handleLogin,
+};
