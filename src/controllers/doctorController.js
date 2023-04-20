19fb13ebb4fd43fe9eb348b9289427e584ce388a
DONAM-IT <doducnam.it@gmail.com>
2023-04-20 22:05:21 +0700

    video 67

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -14,7 +14,35 @@ let getTopDoctorHome = async (req, res) => {
     });
   }
 };
+//req, res là thằng express nó cho chúng ta, nó sẽ handle cho chúng ta
+let getAllDoctors = async (req, res) => {
+  try {
+    let doctors = await doctorService.getAllDoctors();
+    // console.log(doctors);
+    return res.status(200).json(doctors);
+  } catch (error) {
+    console.log(error);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "Error from the server",
+    });
+  }
+};
+let postInforDoctor = async (req, res) => {
+  try {
+    let response = await doctorService.saveDetailInforDoctor(req.body);
+    return res.status(200).json(response); //trả về cho
+  } catch (error) {
+    console.log(error);
+    return res.status(200).json({
+      errCode: -1,
+      errMessage: "Error from the server",
+    });
+  }
+};
 
 module.exports = {
   getTopDoctorHome: getTopDoctorHome,
+  getAllDoctors: getAllDoctors,
+  postInforDoctor: postInforDoctor,
 };
--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -35,6 +35,57 @@ let getTopDoctorHome = (limitInput) => {
   });
 };
 
+let getAllDoctors = () => {
+  //Promise luôn trả về dữ liệu
+  return new Promise(async (resolve, reject) => {
+    try {
+      let doctors = await db.User.findAll({
+        where: { roleId: "R2" },
+        attributes: {
+          exclude: ["password", "image"],
+        },
+      });
+      //resolve trả về 1 object, trong biến object có 1 biến là data chứa tất cả thằng bác sĩ và errCode = 0
+      resolve({
+        errCode: 0,
+        data: doctors,
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+let saveDetailInforDoctor = (inputData) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (
+        !inputData.doctorId ||
+        !inputData.contentHTML ||
+        !inputData.contentMarkdown
+      ) {
+        resolve({
+          errCode: 1,
+          errMessage: "Missing parameter",
+        });
+      } else {
+        await db.Markdown.create({
+          contentHTML: inputData.contentHTML,
+          contentMarkdown: inputData.contentMarkdown,
+          description: inputData.description,
+          doctorId: inputData.doctorId,
+        });
+        resolve({
+          errCode: 0,
+          errMessage: "Save infor doctor succeed!",
+        });
+      }
+    } catch (error) {
+      reject(e);
+    }
+  });
+};
 module.exports = {
   getTopDoctorHome: getTopDoctorHome, //key và value
+  getAllDoctors: getAllDoctors,
+  saveDetailInforDoctor: saveDetailInforDoctor,
 };
