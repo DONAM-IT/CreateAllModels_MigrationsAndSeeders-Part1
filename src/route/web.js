55f7279faa0c4d7fea492b6056c6174c0638628e
DONAM-IT <doducnam.it@gmail.com>
2023-05-04 15:35:49 +0700

    video 90

--- src/route/web.js
+++ src/route/web.js
@@ -3,6 +3,7 @@ import homeController from "../controllers/homeController";
 import userController from "../controllers/userController";
 import doctorController from "../controllers/doctorController";
 import patientController from "../controllers/patientController";
+import specialtyController from "../controllers/specialtyController";
 
 let router = express.Router();
 
@@ -55,6 +56,8 @@ let initWebRoutes = (app) => {
     "/api/verfy-book-appointment",
     patientController.postVerifyBookAppointment
   );
+
+  router.post("/api/create-new-specialty", specialtyController.createSpecialty);
 
   router.get("/hoidanit", (req, res) => {
     return res.send("Hello World with hoidanit");
